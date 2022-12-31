1b7acb9e8fddd6f7c793d532602df20932d0bd3d
Katlego Komane <CynicsTech1998@gmail.com>
2022-12-31 09:59:13 +0200

    first push

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taxi Patal</title>
+</head>
+<body>
+    <div class="main">
+        <form action="controller.php">
+            <h1 class="header">Taxi Patal Form</h1>
+        </form>
+    </div>
+    
+</body>
+</html>
