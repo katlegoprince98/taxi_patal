4e78a767bcf58aa03d7b118f4156a2da958a2071
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-03 18:51:24 +0200

    00

--- content.php
+++ content.php
@@ -8,25 +8,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
-    
     <title>Taxi Patal</title>
 </head>
 <body>
     <div class="main">
-        <form action="controller.php" method="post">
-            <h1 class="header">Taxi Patal Form</h1>
-            <div class="inputs">
-                <input type="text" name="reg" placeholder="Taxi registration">
-                <input type="text" name="driver" placeholder="Taxi driver"><br>
-                <input type="number" name="noSeats" placeholder="Number of seats">
-                <input type="number" name="noPass" placeholder="Number of passengers"><br>
-                <input type="text" name="dest" placeholder="Trip destination">
-                <input type="number" name="price" placeholder="Price per passenger">
-            </div>
-            <div class="button">
-                <button name="submit">CALCULATE</button>
-            </div>
-        </form>
+        
     </div>
     
     
